6a8023a83a5daed246d4ddc1f48b54b9ea489c93
Isaiah Mon Desir <41654564+IsaiahMon@users.noreply.github.com>
2018-11-13 10:06:44 -0500

    Independent C++ files
    Experimental code.

---
+++ cpp-independent/socket-programs/server.cpp
@@ -0,0 +1,12 @@
+/*
+  Name: Isaiah Mon Desir
+  Server-side C++ program to demonstrate Socket programming 
+*/
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+
+
